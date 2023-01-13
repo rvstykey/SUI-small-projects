645625f8b06fad39ed39409fd03b302f1578d7a8
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2023-01-13 16:33:11 +0400

    Weather: Updated to iOS 15, 16 API

--- Weather/Weather/ContentView.swift
+++ Weather/Weather/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            BackgroundView(isNight: $isNight)
+            BackgroundView(isNight: isNight)
             
             VStack {
                 CityTextView(cityName: "Cupertino, CA")
@@ -58,7 +58,7 @@ struct WeatherDayView: View {
                 .font(.system(size: 16, weight: .medium))
                 .foregroundColor(.white)
             Image(systemName: imageName)
-                .renderingMode(.original)
+                .symbolRenderingMode(.multicolor)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40, height: 40)
@@ -70,13 +70,12 @@ struct WeatherDayView: View {
 }
 
 struct BackgroundView: View {
-    @Binding var isNight: Bool
+    var isNight: Bool
     
     var body: some View {
-        LinearGradient(
-            colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")],
-            startPoint: .topLeading, endPoint: .bottomTrailing)
-        .edgesIgnoringSafeArea(.all)
+        ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
     }
 }
 
@@ -98,7 +97,7 @@ struct MainWeatherStatusView: View {
     var body: some View {
         VStack(spacing: 10) {
             Image(systemName: imageName)
-                .renderingMode(.original)
+                .symbolRenderingMode(.multicolor)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 180, height: 180)
--- Weather/Weather/MainButton.swift
+++ Weather/Weather/MainButton.swift
@@ -15,9 +15,15 @@ struct MainButton: View {
     var body: some View {
         Text(title)
             .frame(width: 280, height: 50)
-            .background(backgroundColor)
+            .background(backgroundColor.gradient)
             .foregroundColor(textColor)
             .font(.system(size: 20, weight: .bold))
             .cornerRadius(10)
+    }
+}
+
+struct MainButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MainButton(title: "Test", textColor: .white, backgroundColor: .blue)
     }
 }
