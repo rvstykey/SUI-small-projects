a00a43ffdf58c46bacac58dd98292b02acd73f5d
Rostislav Zapolskiy <rostislav.zapolskiy@gmail.com>
2023-01-15 09:31:13 +0400

    Barcode-Scanner: Added dummy UI

---
+++ Barcode-Scanner/Barcode-Scanner/BarcodeScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Barcode-Scanner
+//
+//  Created by Rostislav Zapolsky on 15.01.23.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Label("Scanned Image", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text("Not Yet Scanned")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct BarcodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
---
+++ Barcode-Scanner/Barcode-Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode-Scanner
+//
+//  Created by Rostislav Zapolsky on 15.01.23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
